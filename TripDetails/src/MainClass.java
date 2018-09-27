d43797e085bc4a98789c8f3073fa45ac107ddf77
RamyaSunkuru <30689615+RamyaSunkuru@users.noreply.github.com>
2018-09-27 17:42:46 +0530

    Update MainClass.java

--- TripDetails/src/MainClass.java
+++ TripDetails/src/MainClass.java
@@ -1,4 +1,24 @@
-
+/*
+Let us say you are building an app for a rideshare company like Uber
+or Ola and you are asked to represent the following entities and their
+relationship using object oriented principles. You can use any object
+oriented language of your choice.
+a. The company hires many drivers.
+b. Capture the name, address, date of birth, license number for
+each driver.
+c. Drivers drive vehicles.
+d. Vehicles can be of type car, bus or auto. The fare to be charged
+depends on the vehicle type.
+e. Driver makes trip. A trip is when the vehicle has a customer
+sitting inside and goes from point A to B. Driver is paid per the
+miles driven while on the trip
+f. Capture the following for each vehicle - vehicle registration
+number, number of miles on odometer, the name of the
+owner.
+g. Capture the following information for each customer - Name,
+address, Credit card info.
+h. Associate the trips to a driver, vehicle and a customer.
+*/
 public class MainClass {
 
 	public static void main(String[] args) {
