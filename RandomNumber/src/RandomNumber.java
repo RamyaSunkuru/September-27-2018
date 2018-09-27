9e2c2c402cce7e12b6929bb6ae0581cb8a24668b
RamyaSunkuru <30689615+RamyaSunkuru@users.noreply.github.com>
2018-09-27 17:47:12 +0530

    Update RandomNumber.java

--- RandomNumber/src/RandomNumber.java
+++ RandomNumber/src/RandomNumber.java
@@ -1,19 +1,17 @@
-
+/*
+If you have a function that generates a random number between 1-
+11, how would you use that function to generate a random number
+between 1-13?
+*/
 public class RandomNumber {
 	static double generateRandom()
 	{
 		double randomValue = Math.random() * 10 + 1;
 		return randomValue;
 	}
-//	static double generateRandomInteger(int min, int max) {
-//		Random r = new Random();
-//		return r. nextInt((max - min) + 1) + min;
-//	}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		//int valueOne =  generateRandomInteger(1,11);
-		//double valueTwo = (valueOne-1 )*(1.2)+1;
 		double valueOne =  generateRandom();
 	    double valueTwo = (valueOne-1 )*(1.2)+1; // multiplied by a factor of (13-1 / 11-1)
 		System.out.println(valueOne);
