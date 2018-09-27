fb79efa9d7d8d07a6c3c71d761588d0d9be4894c
RamyaSunkuru <30689615+RamyaSunkuru@users.noreply.github.com>
2018-09-27 17:40:53 +0530

    Update ArrayOfFifteen.java

--- ArrayOfFifteen/src/ArrayOfFifteen.java
+++ ArrayOfFifteen/src/ArrayOfFifteen.java
@@ -1,4 +1,17 @@
-
+/*
+ii.  x=99
+You are given an array with 15 elements, you don’t know the
+elements of the array. Write code to answer the following questions.
+. Print all the numbers in the array
+a. Find if a number ‘x’ is in the array or not.
+b. Print the position of the number ‘x’
+c. Print true/false for the following question - “does it contain any
+duplicate elements?”
+d. What would the output look like for you program for the following
+array [12, 71, 92, 56, 121, 33, 89, 47, 25, 64, 100, 103, 2, 19, 96]
+i.If x = 47
+ii.  x=99
+*/
 public class ArrayOfFifteen {
 	 static void arrayFunctions(int[]array ,int x) {
 		 boolean isDuplicate = false;
